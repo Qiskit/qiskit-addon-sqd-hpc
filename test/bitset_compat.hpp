62d7ab57dd41bc14a714c50e5b375572610ebf49
Jim Garrison <garrison@ibm.com>
2025-09-19 15:25:55 -0400

    Add tests for compatibility with boost::dynamic_bitset<> (#14)
    * Add tests for boost::dynamic_bitset<>
    
    * Fix when exceptions are disabled
    
    * Use boost-1.89.0 tagged release of each submodule

---
+++ test/bitset_compat.hpp
@@ -0,0 +1,35 @@
+// This code is part of Qiskit.
+//
+// (C) Copyright IBM 2025.
+//
+// This code is licensed under the Apache License, Version 2.0. You may
+// obtain a copy of this license in the LICENSE.txt file in the root directory
+// of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+//
+// Any modifications or derivative works of this code must retain this
+// copyright notice, and modified files need to carry a notice indicating
+// that they have been altered from the originals.
+
+#ifndef BITSET_COMPAT_HPP_
+#define BITSET_COMPAT_HPP_
+
+#include <bitset>
+#include <cassert>
+#include <utility>
+
+#include <boost/dynamic_bitset.hpp>
+
+template <std::size_t N>
+static void set_bitset(unsigned int N_, std::bitset<N> &bitset, unsigned int value)
+{
+    assert(N_ == N);
+    bitset = std::bitset<N>(value);
+}
+
+static void
+set_bitset(unsigned int N, boost::dynamic_bitset<> &bitset, unsigned int value)
+{
+    bitset = std::move(boost::dynamic_bitset<>(N, value));
+}
+
+#endif // BITSET_COMPAT_HPP_
