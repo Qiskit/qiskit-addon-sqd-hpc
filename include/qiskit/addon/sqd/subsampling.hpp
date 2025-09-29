da36218b01562abb51a8e9a89b4c12fcd16b2e8e
Jim Garrison <garrison@ibm.com>
2025-09-29 12:08:28 -0400

    Update formatting

--- include/qiskit/addon/sqd/subsampling.hpp
+++ include/qiskit/addon/sqd/subsampling.hpp
@@ -58,7 +58,8 @@ namespace sqd
 /// @tparam RNGType Type of random number generator.
 template <
     typename BatchVectorType, typename BitstringVectorType, typename WeightVectorType,
-    typename RNGType>
+    typename RNGType
+>
 void subsample(
     BatchVectorType &batch, const BitstringVectorType &bitstrings,
     const WeightVectorType &weights, unsigned int samples_per_batch, RNGType &rng
@@ -150,7 +151,8 @@ BitstringVectorType subsample(
 // NOLINTBEGIN(bugprone-easily-swappable-parameters)
 template <
     typename BatchesVectorType, typename BitstringVectorType, typename WeightVectorType,
-    typename RNGType>
+    typename RNGType
+>
 void subsample_multiple_batches(
     BatchesVectorType &batches, const BitstringVectorType &bitstrings,
     const WeightVectorType &weights, unsigned int samples_per_batch,
